2515b61aa0adc52b9b645767501835a5b0a139d5
Duc <thienductranhuu2784@gmail.com>
2025-10-23 14:57:18 -0400

    attempt to deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // 👈 required for static export (no Image Optimization)
+  },
 };
 
 export default nextConfig;
